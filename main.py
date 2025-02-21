7b73e2512ceacb46df0b3cb927c57946c586c283
Spitfire6272 <bparks@mybce.catholic.edu.au>
2025-02-21 11:48:28 +1000

    Create main.py
    Main file

---
+++ main.py
@@ -0,0 +1,17 @@
+import subprocess
+import ctypes
+import time
+mp3_file = "ping.mp3"#replace with your preferred mp3 file. 
+vlc_path = r"C:\Program Files (x86)\VideoLAN\VLC\vlc.exe"#path to my VLC. Replace as required.
+command = [vlc_path, "--intf", "dummy", mp3_file]
+subprocess.Popen(command, creationflags=subprocess.CREATE_NEW_PROCESS_GROUP)
+def maxxer():
+    APPCOMMAND_VOLUME_UP = 0xA0000
+    WM_APPCOMMAND = 0x319
+    hwnd = ctypes.windll.user32.GetForegroundWindow()
+    for _ in range(50):
+        ctypes.windll.user32.PostMessageW(hwnd, WM_APPCOMMAND, hwnd, APPCOMMAND_VOLUME_UP)
+if __name__ == "__main__":
+    while True:
+        maxxer()
+        time.sleep(0.1)
